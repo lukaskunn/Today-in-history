951e739e3952696ae285f18db53ae93970673402
Lucas Oliveira <lucassioliveira098@gmail.com>
2021-01-25 23:49:48 -0300

    Create index.js
    create index.js with cors to integrate react side

---
+++ server/src/index.js
@@ -0,0 +1,16 @@
+const { request, response } = require('express');
+const express = require('express');
+const routes = require('./routes')
+const cors = require('cors');
+
+const port = process.env.PORT || 3333;
+const app = express();
+
+app.use(cors({ origin: 'http://localhost:3000' }))
+app.use(express.json())
+app.use(routes)
+
+
+app.listen(port, () => {
+    console.log(`server runing on port ${port}`);
+})
