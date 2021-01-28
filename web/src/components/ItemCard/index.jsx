a851323aa2fe9c2fcd9edb5bbdef2327729dcaf3
Lucas Oliveira <lucassioliveira098@gmail.com>
2021-01-28 00:28:18 -0300

    change item card to get data from API

--- web/src/components/ItemCard/index.jsx
+++ web/src/components/ItemCard/index.jsx
@@ -5,8 +5,8 @@ import './styles.css'
 function ItemCard(props) {
     return (
         <div className="item-card">
-                <h3 className="year">year: 1995</h3>
-                <h3 className="description">roma caiu weeeeeeeee</h3>
+                <h3 className="year">{props.year}</h3>
+                <h3 className="description">{props.description}</h3>
         </div>
     )
 }
