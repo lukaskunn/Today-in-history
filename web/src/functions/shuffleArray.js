692701cbdf168399cb6c879d7a4b55dc104898c0
Lucas Oliveira <lucassioliveira098@gmail.com>
2021-02-05 00:03:12 -0300

    Update shuffleArray.js
    add sorting function by the year of objects

--- web/src/functions/shuffleArray.js
+++ web/src/functions/shuffleArray.js
@@ -1,15 +1,30 @@
 function shuffle(array, size) {
-    var newArray = [];
-  
-    for (let i = 0; i < size; i++) {
-      var randomItem = array[Math.floor(Math.random()*array.length)];
-      var index = array.indexOf(randomItem);
-      array.splice(index, 1);
-      newArray.push(randomItem);
-    }
-  
-    newArray.sort();
-    return newArray;
+  var newArray = [];
+
+  for (let i = 0; i < size; i++) {
+    var randomItem = array[Math.floor(Math.random() * array.length)];
+    var index = array.indexOf(randomItem);
+    array.splice(index, 1);
+    newArray.push(randomItem);
   }
-  
-  export default shuffle;
+
+  newArray.sort(dynamicSort('year'));
+  return newArray;
+}
+
+function dynamicSort(property) {
+  var sortOrder = 1;
+  if(property[0] === "-") {
+      sortOrder = -1;
+      property = property.substr(1);
+  }
+  return function (a,b) {
+      /* next line works with strings and numbers, 
+       * and you may want to customize it to your needs
+       */
+      var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+      return result * sortOrder;
+  }
+}
+
+export default shuffle;
