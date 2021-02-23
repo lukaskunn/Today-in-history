79fbb3e2cb4de2e64bde27ba816a6406077f70f3
Lucas Oliveira <lucassioliveira098@gmail.com>
2021-02-23 01:14:33 -0300

    Changes in deaths page

--- web/src/pages/Deaths/index.jsx
+++ web/src/pages/Deaths/index.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Header from '../../components/Header'
+import ItemCard from '../../components/ItemCard'
+import api from '../../services/api'
 
-// import { Container } from './styles';
+import './styles.css'
 
 function Deaths() {
-  return <div />;
+  const [deaths, setDeaths] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+  async function getData(e) {
+    const response = await api.get('/');
+    setDeaths(response.data.deaths);
+  }
+
+  return (
+    <div className="deaths-container">
+      <Header />
+      <main>
+        <fieldset className="deaths">
+          <legend>Deaths</legend>
+          {deaths.map((item, index) => {
+            return <ItemCard year={item.year} description={item.description} wikipedia={item.wikipedia[0].wikipedia} key={index} />
+          })}
+        </fieldset>
+      </main>
+    </div>
+  );
 }
 
 export default Deaths;
