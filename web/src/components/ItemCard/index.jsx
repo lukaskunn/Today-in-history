d532e4a2b9cb64c705bd20845da04da8ec4ae851
Lucas Oliveira <lucassioliveira098@gmail.com>
2021-02-23 01:13:28 -0300

    Changes in ItemCard Component

--- web/src/components/ItemCard/index.jsx
+++ web/src/components/ItemCard/index.jsx
@@ -4,17 +4,19 @@ import './styles.css'
 
 function ItemCard(props) {
     return (
-        <div className="item-card">
-            <div className="image">
-                <p>imagem </p>
+        <a href={`${props.wikipedia}`} target="_blank" rel="noopener noreferrer">
+            <div className="item-card">
+                <div className="image">
+                    <p>imagem </p>
+                </div>
+                <div className="content">
+                    <h2 className="year">{props.year}</h2>
+                    <h4 className="description">
+                        {props.description}
+                    </h4>
+                </div>
             </div>
-            <div className="content">
-                <h2 className="year">{props.year}</h2>
-                <h4 className="description">
-                    {props.description}
-            </h4>
-            </div>
-        </div>
+        </a>
     )
 }
 
