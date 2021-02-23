4703cb1584c735a822031e210cef58f9e4e3daec
Lucas Oliveira <lucassioliveira098@gmail.com>
2021-02-23 01:14:43 -0300

    changes in Events page

--- web/src/pages/Events/index.jsx
+++ web/src/pages/Events/index.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Header from '../../components/Header'
+import ItemCard from '../../components/ItemCard'
+import api from '../../services/api'
 
-// import { Container } from './styles';
+import './styles.css'
 
 function Events() {
-  return <div />;
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+  async function getData(e) {
+    const response = await api.get('/');
+    setEvents(response.data.events);
+  }
+
+  return (
+    <div className="events-container">
+      <Header />
+      <main>
+        <fieldset className="events">
+          <legend>Events</legend>
+          {events.map((item, index) => {
+            return <ItemCard year={item.year} description={item.description} wikipedia={item.wikipedia[0].wikipedia} key={index} />
+          })}
+        </fieldset>
+      </main>
+    </div>
+
+  );
 }
 
 export default Events;
