97a05627274b75489eac025930c3d6e3a0f980d1
lucas silva oliveira <lucassioliveira098@gmail.com>
2021-03-17 01:07:54 -0300

    Add Background image in each Item event card

--- web/src/components/ItemCard/index.jsx
+++ web/src/components/ItemCard/index.jsx
@@ -1,23 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import './styles.css'
+import "./styles.css";
 
 function ItemCard(props) {
-    return (
-        <a href={`${props.wikipedia}`} target="_blank" rel="noopener noreferrer">
-            <div className="item-card">
-                <div className="image">
-                    <p>imagem </p>
-                </div>
-                <div className="content">
-                    <h2 className="year">{props.year}</h2>
-                    <h4 className="description">
-                        {props.description}
-                    </h4>
-                </div>
-            </div>
-        </a>
-    )
+  return (
+    <a
+      href={`${props.wikipedia}`}
+      target="_blank"
+      rel="noopener noreferrer"
+      url_image={props.url_image}
+    >
+      <div
+        className="item-card"
+        style={{
+          backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url(${props.background})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center"
+        }}
+      >
+        <div className="content">
+          <h2 className="year">{props.year}</h2>
+          <h4 className="description">{props.description}</h4>
+        </div>
+      </div>
+    </a>
+  );
 }
 
 export default ItemCard;
