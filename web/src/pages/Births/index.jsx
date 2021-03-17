e119bd5a34c24319b6fcaf5a332a3a53226ebbf4
lucas silva oliveira <lucassioliveira098@gmail.com>
2021-03-17 01:09:25 -0300

    update births page
    add date in top of the page and add background image to events cards

--- web/src/pages/Births/index.jsx
+++ web/src/pages/Births/index.jsx
@@ -1,20 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import Header from '../../components/Header'
-import ItemCard from '../../components/ItemCard'
-import ReturnButton from '../../components/ReturnButton'
+import React, { useState, useEffect } from "react";
+import Header from "../../components/Header";
+import ItemCard from "../../components/ItemCard";
+import ReturnButton from "../../components/ReturnButton";
+import getAtualDate from "../../functions/getAtualDate";
 
-import api from '../../services/api'
-import './styles.css'
+import api from "../../services/api";
+import "./styles.css";
 
 function Births() {
   const [births, setBirths] = useState([]);
-
+  const [date, setDate] = useState();
   useEffect(() => {
     getData();
-  }, [])
+    setDate(getAtualDate());
+  }, []);
 
   async function getData(e) {
-    const response = await api.get('/');
+    const response = await api.get("/");
     setBirths(response.data.births);
   }
 
@@ -22,16 +24,24 @@ function Births() {
     <div className="births-container">
       <Header />
       <main>
-      <ReturnButton />
+        <ReturnButton />
+        <h1>{date}</h1>
         <fieldset className="births">
           <legend>Births</legend>
           {births.map((item, index) => {
-            return <ItemCard year={item.year} description={item.description} wikipedia={item.wikipedia[0].wikipedia} key={index} />
+            return (
+              <ItemCard
+                year={item.year}
+                description={item.description}
+                wikipedia={item.wikipedia[0].wikipedia}
+                key={index}
+                background={item.url_image}
+              />
+            );
           })}
         </fieldset>
       </main>
     </div>
-
   );
 }
 
