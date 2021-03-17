975cccdc6e39f712486b1d22aa8f4d4f47e0313a
lucas silva oliveira <lucassioliveira098@gmail.com>
2021-03-17 01:09:33 -0300

    update deaths page
    add date in top of the page and add background image to events cards

--- web/src/pages/Deaths/index.jsx
+++ web/src/pages/Deaths/index.jsx
@@ -1,20 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import Header from '../../components/Header'
-import ItemCard from '../../components/ItemCard'
-import api from '../../services/api'
-import ReturnButton from '../../components/ReturnButton'
+import React, { useState, useEffect } from "react";
+import Header from "../../components/Header";
+import ItemCard from "../../components/ItemCard";
+import api from "../../services/api";
+import ReturnButton from "../../components/ReturnButton";
+import getAtualDate from '../../functions/getAtualDate'
 
-import './styles.css'
+import "./styles.css";
 
 function Deaths() {
   const [deaths, setDeaths] = useState([]);
+  const [date, setDate] = useState();
 
   useEffect(() => {
     getData();
-  }, [])
+    setDate(getAtualDate());
+  }, []);
 
   async function getData(e) {
-    const response = await api.get('/');
+    const response = await api.get("/");
     setDeaths(response.data.deaths);
   }
 
@@ -23,10 +26,19 @@ function Deaths() {
       <Header />
       <main>
         <ReturnButton />
+        <h1>{date}</h1>
         <fieldset className="deaths">
           <legend>Deaths</legend>
           {deaths.map((item, index) => {
-            return <ItemCard year={item.year} description={item.description} wikipedia={item.wikipedia[0].wikipedia} key={index} />
+            return (
+              <ItemCard
+                year={item.year}
+                description={item.description}
+                wikipedia={item.wikipedia[0].wikipedia}
+                key={index}
+                background={item.url_image}
+              />
+            );
           })}
         </fieldset>
       </main>
